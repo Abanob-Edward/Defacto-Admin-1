6b6932dc2dc1143bd6f5a3a6346c6e4b083c93a8
hashem mohamed <hm.cassem@gmail.com>
2024-03-25 05:21:08 +0200

    Created

---
+++ Defacto-Front/src/app/Models/iregister.ts
@@ -0,0 +1,11 @@
+export interface IRegister {
+  userName: string;
+  name: string;
+  email: string;
+  streetAddress: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+  password: string;
+}
--- Defacto-Front/src/app/app.component.ts
+++ Defacto-Front/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { SideNavComponent } from './component/side-nav/side-nav.component';
+import { ProductComponent } from './component/product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,SideNavComponent,ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
